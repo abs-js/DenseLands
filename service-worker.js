790ea1794e9754ab00d7c2113683a1e5abb22fbb
abs-j <artur.2012.bernardo@gmail.com>
2026-09-13 14:07:19 -0300

    Criar service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE = "denselands-v1";
+
+
+self.addEventListener(
+"install",
+event => {
+
+event.waitUntil(
+caches.open(CACHE)
+.then(cache =>
+cache.addAll([
+"./",
+"./index.html",
+"./manifest.json",
+"./icon.svg"
+])
+)
+
+);
+
+});
+
+
+self.addEventListener(
+"fetch",
+event => {
+
+event.respondWith(
+caches.match(event.request)
+.then(r => r || fetch(event.request))
+);
+
+});
