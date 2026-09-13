52d6108467b219d12e87152ad35327820770b9d0
abs-j <artur.2012.bernardo@gmail.com>
2026-09-13 14:14:16 -0300

    Fix menus off-screen on mobile; bump SW cache so HTML updates.

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,39 @@
-const CACHE = "denselands-v1";
+const CACHE = "denselands-v2";
 
-
-self.addEventListener(
-"install",
-event => {
-
-event.waitUntil(
-caches.open(CACHE)
-.then(cache =>
-cache.addAll([
-"./",
-"./index.html",
-"./manifest.json",
-"./icon.svg"
-])
-)
-
-);
-
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE).then((cache) =>
+      cache.addAll(["./", "./index.html", "./manifest.json", "./icon.svg"])
+    )
+  );
 });
 
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
 
-self.addEventListener(
-"fetch",
-event => {
-
-event.respondWith(
-caches.match(event.request)
-.then(r => r || fetch(event.request))
-);
-
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  const isPage =
+    req.mode === "navigate" ||
+    req.destination === "document" ||
+    (req.url && req.url.indexOf("index.html") >= 0);
+  if (isPage) {
+    event.respondWith(
+      fetch(req)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE).then((cache) => cache.put(req, copy));
+          return res;
+        })
+        .catch(() => caches.match(req).then((r) => r || caches.match("./index.html")))
+    );
+    return;
+  }
+  event.respondWith(caches.match(req).then((r) => r || fetch(req)));
 });
